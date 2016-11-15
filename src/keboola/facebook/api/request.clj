fce8775f59129c75e0d4f29d17f797369516f31b
Tomas Kacur <tomas.kacur@keboola.com>
2016-11-15 19:28:29 +0100

    feat: nested posts request

--- src/keboola/facebook/api/request.clj
+++ src/keboola/facebook/api/request.clj
@@ -6,24 +6,28 @@
 (def default-version "v2.8")
 
 (defn get-next-page-url [response]
-  (get-in response [:body :paging :next]))
+  (get-in response [:paging :next]))
 
 (defn get-response-data [response]
-  (get-in response [:body :data])
+  (get-in response [:data])
   )
 
 (defn collect-result [response api-fn]
+  ;(println (keys response) (:body (api-fn (get-next-page-url response))))
   (lazy-seq
    (if (get-next-page-url response)
      (concat (get-response-data response)
-             (collect-result (api-fn (get-next-page-url response)) api-fn)))))
+             (collect-result
+              (:body (api-fn (get-next-page-url response)))
+              api-fn
+              )))))
 
 
 (defn make-url [path & {:keys [version]}]
   (str graph-api-url (or version default-version) "/" path))
 
 
-(defn get-request-old [access-token path & {:keys [query version]}]
+(defn get-request [access-token path & {:keys [query version]}]
   (let [query-params (assoc query :access_token access-token)
         request-fn (fn [url] (client/GET url :query-params query-params :as :json))]
       (collect-result
@@ -31,10 +35,18 @@
        request-fn
        )))
 
-(defn get-request [access-token path & {:keys [query version]}]
+(defn nested-request [access-token path & {:keys [query version]}]
   (let [query-params (assoc query :access_token access-token :method "GET")
-        request-fn (fn [url] (client/POST url :form-params query-params :as :json))]
-    (collect-result
-     (request-fn (make-url path :version version))
-     request-fn))
-  )
+        full-url (make-url path :version version)
+        request-fn (fn [url] (client/POST url :form-params query-params :as :json))
+        response (request-fn full-url)
+        ]
+    ;(println (map #(first %) (:body response)))
+    (map
+     #(hash-map
+       :account-id (first %)
+       :data (collect-result
+              (-> % second :posts)
+              (fn [url] (client/GET url :query-params {:access_token access-token} :as :json))
+               ))
+     (:body response))))
