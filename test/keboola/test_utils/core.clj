8a74e50ec963d31645f3068b2a7f978cf61225aa
Tomas Kacur <tomas.kacur@keboola.com>
2016-11-24 17:59:34 +0100

    feat(test_utils): temp dir, delete dir recursively

--- test/keboola/test_utils/core.clj
+++ test/keboola/test_utils/core.clj
@@ -1,6 +1,6 @@
 (ns keboola.test-utils.core
   (:require [clojure.test :refer [is]] )
-  (:require [clojure.string])
+  (:require [clojure.string :as string])
   )
 
 (defmacro with-err-str
@@ -20,3 +20,35 @@
 
 (defn prints-msg? [print-fn & params]
   (is (= (clojure.string/trim (with-out-str (apply print-fn params))) (clojure.string/join " " params))))
+
+(defn mk-path [& args]
+  (string/join "/" args))
+
+(defn mk-tmp-dir!
+  "Creates a unique temporary directory on the filesystem. Typically in /tmp on
+  *NIX systems. Returns a File object pointing to the new directory. Raises an
+  exception if the directory couldn't be created after 10000 tries."
+  [prefix]
+  (let [base-dir (java.io.File. (System/getProperty "java.io.tmpdir"))
+        base-name (str prefix (java.util.Date.) "-" (long (rand 100)) "-")
+        tmp-base (mk-path base-dir base-name)
+        max-attempts 10000]
+    (loop [num-attempts 1]
+      (if (= num-attempts max-attempts)
+        (throw (Exception. (str "Failed to create temporary directory after " max-attempts " attempts.")))
+        (let [tmp-dir-name (str tmp-base num-attempts)
+              tmp-dir (java.io.File. tmp-dir-name)]
+          (if (.mkdir tmp-dir)
+            tmp-dir
+            (recur (inc num-attempts))))))))
+
+
+(defn
+^{ :doc "Deletes the given directory even if it contains files or subdirectories. This function will attempt to delete
+all of the files and directories in the given directory first, before deleting the directory. If the directory cannot be
+deleted, this function aborts and returns nil. If the delete finishes successfully, then this function returns true." }
+  recursive-delete [directory]
+  (if (.isDirectory directory)
+    (when (reduce #(and %1 (recursive-delete %2)) true (.listFiles directory))
+      (.delete directory))
+    (.delete directory)))
