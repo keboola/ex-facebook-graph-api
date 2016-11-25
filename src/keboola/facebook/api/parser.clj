26258d5a535485e94f423d0a84695797ebd9a64d
Tomas Kacur <tomas.kacur@keboola.com>
2016-11-25 18:31:43 +0100

    refactoring sort unused functions

--- src/keboola/facebook/api/parser.clj
+++ src/keboola/facebook/api/parser.clj
@@ -1,6 +1,57 @@
 (ns keboola.facebook.api.parser)
 
+(defn unfold-nested-sequence
+  "flatten sequence of sequences(of maps) into sequence of maps
+  returns lazy sequence of maps"
+  [data]
+  ((fn step [item other]
+     (if (some? item)
+         (lazy-seq
+          (if (map? item)
+            (cons item (step (first other) (rest other)))
+            (step (first item) (concat (rest item) other))
+            )))
+     ) (first data) (rest data)))
 
+
+(defn get-columns
+  "reducer fn - extract all columns used in items for each table used
+  result sample: {:columns {posts: #{id, message, story, create_time}}}"
+  [item memo]
+  (let [
+        table-row (dissoc item :keboola)
+        table-name (-> item :keboola :table-name)
+        columns (:columns memo {})
+        table-columns (columns table-name #{})
+        new-table-columns (clojure.set/union table-columns (into #{} (keys table-row)))
+        new-columns (assoc columns table-name new-table-columns)
+        ]
+    (assoc memo :columns new-columns )))
+
+(defn analyze
+  "reducer fn - analyze items, returns all parent-types and tables columns used
+  result {:parent-types <set_of_parent_types> :columns <map_of_tables_with_columns>}"
+  [item memo]
+  (let [
+        parent-types (:parent-types memo {})
+        parent-type-name (-> item :keboola :parent-type)
+        parent-type-count (parent-types parent-type-name 0)]
+    (get-columns item
+     (assoc memo :parent-types (assoc parent-types parent-type-name (inc parent-type-count))))))
+
+(defn analyze-seq
+  ([data] (analyze-seq data 0))
+  ([data max-iter-count]
+   (loop [memo (analyze (first data) {})
+          other (rest data)
+          cnt 1
+          ]
+     (if (and (not= cnt max-iter-count) (some? (first other)))
+       (recur (analyze (first other) memo) (rest other) (inc cnt))
+       (assoc memo :cnt cnt)))))
+
+
+;;;; NOT USED:
 (defn reduce-result-sequence
   "traverse response from nested-request
   and calls given reducer reduceFn(item, memo) for each
@@ -17,47 +68,6 @@
       :else memo
       )))
 
-(defn unfold-nested-sequence [data]
-  ((fn step [item other]
-     (if (some? item)
-         (lazy-seq
-          (if (map? item)
-            (cons item (step (first other) (rest other)))
-            (step (first item) (concat (rest item) other))
-            )))
-     ) (first data) (rest data)))
-
-
-;;;;********REDUCER Functions****
-;;; use them as a param for reduce-result-sequence
-;;; e.g (reduce-result-sequence data analyze)
-
-(defn get-columns
-  "reducer fn - extract all columns used in items for each table used
-  result sample: {:columns {posts: #{id, message, story, create_time}}}"
-  [item memo]
-  (let [
-        table-row (dissoc item :keboola)
-        table-name (-> item :keboola :table-name)
-        columns (:columns memo {})
-        table-columns (columns table-name #{})
-        new-table-columns (clojure.set/union table-columns (into #{} (keys table-row)))
-        new-columns (assoc columns table-name new-table-columns)
-        ]
-    (assoc memo :columns new-columns )))
-
-
-(defn analyze
-  "reducer fn - analyze items, returns all parent-types and tables columns used
-  result {:parent-types <set_of_parent_types> :columns <map_of_tables_with_columns>}"
-  [item memo]
-  (let [
-        parent-types (:parent-types memo {})
-        parent-type-name (-> item :keboola :parent-type)
-        parent-type-count (parent-types parent-type-name 0)]
-    (get-columns item
-     (assoc memo :parent-types (assoc parent-types parent-type-name (inc parent-type-count))))))
-
 (defn filter-table-data [table-name item memo]
   (let [items (:items memo (lazy-seq []))
         cnt (:cnt memo 0)
@@ -65,17 +75,6 @@
     (if (= (-> item :keboola :table-name) table-name)
       (assoc memo  :items (cons item items) :cnt (inc cnt))
       memo)))
-
-(defn get-x-items
-  "reducer fn - extracts x number of items and returns them under :items keyword.
-  if x == 0 then never stop
-  result: {:cnt <parsed_items_count> :stop true :items <lazy_seq_of_items>}"
-  [x item memo]
-  (if-let [cnt (:cnt memo)]
-    (cond
-      (= x cnt) (assoc memo :stop true :items (cons item (:items memo) ))
-      :else  (assoc memo :cnt (inc cnt) :stop false :items (cons item (:items memo))))
-    {:cnt 1 :stop false :items (lazy-seq)}))
 
 (defn stop-after-x-items
   "reducer fn - make reducing fn stop after x items extracted.
@@ -89,6 +88,17 @@
     {:cnt 1 :stop false}))
 
 
+(defn get-x-items
+  "reducer fn - extracts x number of items and returns them under :items keyword.
+  if x == 0 then never stop
+  result: {:cnt <parsed_items_count> :stop true :items <lazy_seq_of_items>}"
+  [x item memo]
+  (if-let [cnt (:cnt memo)]
+    (cond
+      (= x cnt) (assoc memo :stop true :items (cons item (:items memo) ))
+      :else  (assoc memo :cnt (inc cnt) :stop false :items (cons item (:items memo))))
+    {:cnt 1 :stop false :items (lazy-seq)}))
+
 (defn chain-fns
   "reducer fn - chains reducer fns and passes curent memo
   example: (chain-fns (partial stop-after-x-times 4) analyze) - will cala analyze on 4 items"
@@ -101,7 +111,6 @@
       (if (nil? f) current-memo
           (recur (first rest-fns) (rest rest-fns) (f item current-memo))))))
 
-
 (defn reduce-and-analyze
   "reduce-sequence-result called with chained analyze and stop-after-x-items
   returns analyzed sequenced data same as analyze reduce fn"
@@ -109,15 +118,3 @@
   (let [max-count (if (some? max-items-count) (apply identity max-items-count) 0)
         reduce-fn (chain-fns (partial stop-after-x-items max-count) analyze)]
     (reduce-result-sequence data reduce-fn)))
-
-
-(defn analyze-seq
-  ([data] (analyze-seq data 0))
-  ([data max-iter-count]
-   (loop [memo (analyze (first data) {})
-          other (rest data)
-          cnt 1
-          ]
-     (if (and (not= cnt max-iter-count) (some? (first other)))
-       (recur (analyze (first other) memo) (rest other) (inc cnt))
-       (assoc memo :cnt cnt)))))
