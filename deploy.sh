314bb44694ebfd3cb74d1301d44a7f34d547c1f0
Tomas Kacur <tomas.kacur@keboola.com>
2016-12-15 12:40:35 +0100

    fix deply.sh built image name

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag exfacebookgraphapi_app quay.io/keboola/ex-facebook-graph-api:$TRAVIS_TAG
+docker tag keboola/ex-facebook-insights quay.io/keboola/ex-facebook-graph-api:$TRAVIS_TAG
 docker push quay.io/keboola/ex-facebook-graph-api:$TRAVIS_TAG
