4e89c49fcd9f8ee3ed122ecadbc5918094530aef
Tomas Kacur <tomas.kacur@keboola.com>
2016-12-16 20:21:15 +0100

    test parsing insights metrics data

--- test/keboola/facebook/api/request_test.clj
+++ test/keboola/facebook/api/request_test.clj
@@ -13,12 +13,17 @@
 (deftest test-get-nested-object
   (is (test-and-check `sut/get-nested-objects 10)))
 
-(deftest test-flatten-object
-  (is (test-and-check `sut/flatten-object 20)))
+(deftest test-flatten-value-object
+  (is (test-and-check `sut/flatten-value-object 20)))
 
-(deftest test-flatten-array
-  (is (test-and-check `sut/flatten-array 20))
-  )
+(deftest test-flatten-array-value
+  (is (test-and-check `sut/flatten-array-value 20)))
 
-(deftest test-filter-values
-  (is (test-and-check `sut/filter-values 2)))
+(deftest test-filter-scalars
+  (is (test-and-check `sut/filter-scalars 20)))
+
+(deftest test-filter-flatten-objects
+  (is (test-and-check `sut/filter-flatten-objects 20)))
+
+(deftest test-flatte-array
+  (is (test-and-check `sut/flatten-array 10)))
