df5c69bddaf6d3a8cc302259f2f4614d6d9b608d
Tomas Kacur <tomas.kacur@keboola.com>
2017-01-05 00:22:36 +0100

    :zap: analyze structure per table rows and not all rows

---
+++ src/keboola/facebook/insights_extractor/output.clj
@@ -0,0 +1,48 @@
+(ns keboola.facebook.insights-extractor.output
+  (:require [keboola.utils.json-to-csv :as csv]
+            [keboola.docker.runtime :as runtime]
+            [clojure.core.async :as async]))
+
+(defn sort-columns [columns]
+  (let [prefered-columns [:id :account-id :parent-type :parent-id :name :key1 :key2 :value :period :end_time :title]
+        used-prefered-columns (filter (set columns) prefered-columns)
+        other-columns (filter #(not ((set used-prefered-columns) %)) columns)]
+    (concat used-prefered-columns (sort other-columns))))
+
+(defn get-primary-key [table-columns]
+  (let [basic-pk ["id"]
+        extended-pk ["key1" "key2" "end_time"]]
+    (concat basic-pk (filter (fn [column] (some #(= % (keyword column)) table-columns)) extended-pk))))
+
+(defn write-one-table [table-name table-rows file-path-prefix]
+  (let [sample-rows (take 500 table-rows)
+        sample-columns (set (mapcat #(-> % (dissoc :keboola) keys) sample-rows))
+        has-data-columns (not-empty (disj sample-columns :id :account-id))
+        columns (conj sample-columns :parent-id :parent-type :account-id)
+        csv-header (sort-columns columns)
+        manifest {:incremental true :primary_key (get-primary-key columns)}
+        file-path (str file-path-prefix "_" table-name)]
+    (if has-data-columns
+      (do
+        (runtime/log-strings "Start writing csv output of table" table-name "with columns" csv-header)
+        (runtime/save-manifest file-path manifest)
+        (csv/write file-path csv-header
+                   (map #(assoc %
+                                :account-id (-> % :keboola :account-id)
+                                :parent-id (-> % :keboola :parent-id)
+                                :parent-type (-> % :keboola :parent-type))
+                        table-rows)))
+      (runtime/log-strings "Skipping table" table-name "with non data columns" csv-header))))
+
+(defn get-table-name [row]
+  (-> row :keboola :table-name))
+
+(defn filter-table-rows [table-name rows]
+  (filter #(= table-name (get-table-name %)) rows))
+
+(defn create-write-channels [rows file-path-prefix]
+  (let [sample (take 3000 rows)
+        tables-names (set (map #(get-table-name %) sample))]
+    (runtime/log-strings "found tables" tables-names)
+    (map #(async/thread (write-one-table % (filter-table-rows % rows) file-path-prefix) true)
+         tables-names)))
--- src/keboola/facebook/insights_extractor/query.clj
+++ src/keboola/facebook/insights_extractor/query.clj
@@ -1,53 +1,18 @@
 (ns keboola.facebook.insights-extractor.query
   (:gen-class)
   (:require [keboola.facebook.api.request :as request]
-            [keboola.facebook.api.parser :as parser]
-            [cheshire.core :refer [parse-string]]
-            [keboola.utils.json-to-csv :as csv]
+            [keboola.facebook.insights-extractor.output :as output]
             [keboola.docker.runtime :as runtime]
-            [clojure.core.async
-             :as async
-             :refer [>! <! >!! <!! go chan buffer close! thread
-                     alts! alts!! timeout]]))
+            [clojure.core.async :as async]))
 
-(defn get-primary-key [table-columns]
-  (let [basic-pk ["id"]
-        extended-pk ["key1" "key2" "end_time"]]
-    (concat basic-pk (filter (fn [column] (some #(= % (keyword column)) table-columns)) extended-pk))))
-
-(defn make-csv-write-chan [get-data-fn columns filepath]
-  (let [manifest {:incremental true :primary_key (get-primary-key columns)}]
-    (runtime/save-manifest filepath manifest)
-    (async/thread
-      (csv/write filepath columns (get-data-fn))
-      true)))
-
-(defn filter-table-data-fn [data table-name]
-  (fn []
-    (map #(assoc %
-                 :account-id (-> % :keboola :account-id)
-                 :parent-id (-> % :keboola :parent-id)
-                 :parent-type (-> % :keboola :parent-type))
-         (filter #(= table-name (-> % :keboola :table-name)) data))))
-
-(defn filter-non-ids-only-columns [tables-columns-map]
-  (let [ids-only-columns #{:id :account-id}]
-    (into {} (filter #(not (every? ids-only-columns (second %))) tables-columns-map))))
 
 (defn run-nested-query [token out-dir {:keys [name query version]}]
   (let [nested-data (request/nested-request token query :version version)
-        lazy-data-seq (apply concat nested-data)
-        analyzed-structure (parser/analyze-seq lazy-data-seq 3000)
-        tables-columns (filter-non-ids-only-columns (:columns analyzed-structure))
-        write-channels (map (fn [[table-name columns]]
-                              (make-csv-write-chan
-                               (filter-table-data-fn lazy-data-seq table-name)
-                               (conj columns :parent-id :parent-type :account-id)
-                               (str out-dir name "_" table-name))) tables-columns)]
-    (runtime/log-strings "Writing output to csv files. Analyzed structure:" analyzed-structure)
+        all-rows (apply concat nested-data)
+        write-channels (output/create-write-channels all-rows (str out-dir name))]
+    (runtime/log-strings "Parsing started")
     (mapv #(async/<!! %) write-channels)
-    (runtime/log-strings "Run query " name " finished" )
-    ))
+    (runtime/log-strings "Run query " name " finished" )))
 
 (defn parse-token [credentials]
   (:token credentials))
