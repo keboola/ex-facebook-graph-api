c0d47498c0d678f5f1f1756e5452e3d895663687
Tomas Kacur <tomas.kacur@keboola.com>
2017-04-07 15:42:53 +0200

    :cop: failing pk tests

---
+++ test/keboola/facebook/insights_extractor/output_test.clj
@@ -0,0 +1,20 @@
+(ns keboola.facebook.insights-extractor.output-test
+  (:require [keboola.facebook.insights-extractor.output :as sut]
+            [clojure.test :refer :all]))
+
+(defn test-pk [value expected]
+  (let [complete-expected (conj expected "parent_id")]
+    (is (= (count value) (count complete-expected)))
+    (is (= (set value) (set complete-expected)))))
+
+(deftest test-primary-key
+  (test-pk (sut/get-primary-key ["id"] "") [])
+  (test-pk (sut/get-primary-key [:id] "") ["id"])
+  (test-pk (sut/get-primary-key ["foo"] "") [])
+  (test-pk (sut/get-primary-key [:foo] "") [])
+  (test-pk (sut/get-primary-key [:id :key] "") ["id"])
+  (test-pk (sut/get-primary-key [:id :key1 :foo :key2] "insights") ["id" "key1" "key2"])
+  (test-pk (sut/get-primary-key [:id :placement :foo :key2] "insights") ["id" "placement" "key2"])
+  (test-pk (sut/get-primary-key [:id :placement :foo :key2] "") ["id" ])
+  (test-pk (sut/get-primary-key [:id :key1 :foo :key2] "") ["id"])
+    )
