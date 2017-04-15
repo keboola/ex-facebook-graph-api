feae80e79de0e0c474344a02eb7c9c124de39f9b
Tomas Kacur <tomas.kacur@keboola.com>
2017-04-15 19:28:31 +0200

    :cop: feed summary regression tests

---
+++ test/keboola/regression_tests/feedsummary/test_feedsummary.clj
@@ -0,0 +1,21 @@
+(ns keboola.regression-tests.feedsummary.test-feedsummary
+  (:require [keboola.regression-tests.feedsummary.apicalls :as apicalls]
+            [clojure.test :as t :refer :all]
+            [keboola.regression-tests.outdirs-check :as outdirs-check]
+            [keboola.test-utils.core :as test-utils]
+            [keboola.facebook.insights-extractor.output :refer [reset-columns-map]]
+            [keboola.facebook.insights-extractor.core :refer [prepare-and-run]]
+            )
+  (:use clj-http.fake))
+
+(deftest feedsummary-test
+  (let [tmp-dir (.getPath (test-utils/mk-tmp-dir! "feedsummary"))]
+    (println "testing dir:" tmp-dir)
+    (println "expected dir:" "test/keboola/regression_tests/feedsummary")
+    (test-utils/copy-config-tmp "test/keboola/regression_tests/feedsummary" tmp-dir)
+    (with-global-fake-routes-in-isolation
+      apicalls/recorded
+      (reset-columns-map)
+      (prepare-and-run tmp-dir)
+      (outdirs-check/is-equal "test/keboola/regression_tests/feedsummary" tmp-dir)
+      )))
