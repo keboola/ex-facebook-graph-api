6ba250a67d5a873d2832aaa5c1fcabe01d7f22c2
Tomas Kacur <tomas.kacur@keboola.com>
2017-07-10 18:51:46 +0800

    :bug: parse post properties as array FIXES #17

--- src/keboola/facebook/api/parser.clj
+++ src/keboola/facebook/api/parser.clj
@@ -33,7 +33,7 @@
      :data {:data [(:summary object)]}
      :parent-id (or (:id row) (:parent-id params))
      :fb-graph-node (str (:fb-graph-node params) "_" object-name)}))
-     
+
 
 
 (s/fdef get-nested-objects
@@ -53,7 +53,7 @@
                                             :data v
                                             :parent-id (or (:id row) (:parent-id params))
                                             :fb-graph-node (str (:fb-graph-node params) "_" (name k))}
-                                            
+
                                            (extract-summary (name k) v row params)) m))
                                  [] row)]
        (concat memo (keep identity objects))
@@ -85,7 +85,7 @@
      (map? item) (mapcat (fn [[key1 val]] (flatten-value-object (name key1) val)) item)
      :else (list {:key1 "" :key2 "" :value item}))))
 
-(def ads-action-stats-types #{:actions
+(def ads-action-stats-types #{:actions :properties
                               :action_values :canvas_component_avg_pct_view
                               :cost_per_10_sec_video_view :cost_per_action_type :cost_per_unique_action_type
                               :unique_actions :video_10_sec_watched_actions :video_15_sec_watched_actions
