ac5e15e3bd97dd6511a3a9eb0fbfdbb48257fe23
odinuv <odinuv@gmail.com>
2017-08-09 09:59:45 +0200

    fix: typo

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ docker tag ${KBC_APP_REPOSITORY}:latest ${REPOSITORY}:${TRAVIS_TAG}
 docker tag ${KBC_APP_REPOSITORY}:latest ${REPOSITORY}:latest
 eval $(docker run --rm \
     -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
-    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP_1)})
+    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP_1})
 docker push ${REPOSITORY}:${TRAVIS_TAG}
 docker push ${REPOSITORY}:latest
 
