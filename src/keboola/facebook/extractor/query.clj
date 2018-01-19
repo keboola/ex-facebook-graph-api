ca26b7923946115b56b16bb6709b6296a9e3c9e8
Tomas Kacur <tomas.kacur@keboola.com>
2018-01-19 12:36:21 +0100

    :zap: run query with page token only for insights retrieval

--- src/keboola/facebook/extractor/query.clj
+++ src/keboola/facebook/extractor/query.clj
@@ -4,12 +4,21 @@
             [clojure.string :as s]
             [keboola.docker.runtime :as runtime]
             [keboola.facebook.api.request :as request]
-            [keboola.facebook.extractor.output :as output]))
+            [keboola.facebook.extractor.output :as output]
+            [clojure.string :as string]))
 
 (defn- run-and-write [token out-dir prefix query version]
   (let [nested-data (request/nested-request token query :version version)
         all-rows (apply concat nested-data)]
     (output/write-rows all-rows out-dir prefix)))
+
+
+(defn retrieve-page-access-token [id token version]
+  (let [accounts (request/get-accounts token :version version)
+        page-token (:access_token (first (filter #(= id (:id %)) accounts)))]
+    (if (nil? page-token)
+      (runtime/log-strings "Could not find page access token for" id "Will use user token instead."))
+    page-token))
 
 (defn run-nested-query [token out-dir {:keys [name query version]}]
   (if-let [ids-str (:ids query)]
@@ -19,14 +28,7 @@
     (run-and-write token out-dir name query version))
   (runtime/log-strings "Run query" name "finished"))
 
-(defn retrieve-page-access-token [id token version]
-  (let [accounts (request/get-accounts token :version version)
-        page-token (:access_token (first (filter #(= id (:id %)) accounts)))]
-    (if (nil? page-token)
-      (runtime/log-strings "Could not find page access token for" id "Will use user token instead."))
-    page-token))
-
-(defn run-nested-query-per-id [user-token out-dir {:keys [name query version]}]
+(defn run-nested-query-with-page-token [user-token out-dir {:keys [name query version]}]
   (if-let [ids-str (:ids query)]
     (doseq [id (s/split ids-str #",")]
       (let [token (or (retrieve-page-access-token id user-token version) user-token)]
@@ -46,15 +48,21 @@
         ;else return query untouched
         query)))
 
-(defn ex-fb? []
-  (= "keboola.ex-facebook" (runtime/get-component-id)))
+(defn query-contains-insights? [{:keys [fields path]}]
+  (or (string/includes? fields "insights")
+      (string/includes? path "insights")))
+
+(defn need-page-token? [query]
+  (and (= "keboola.ex-facebook" (runtime/get-component-id))
+       (query-contains-insights? query)))
 
 (defn run-query [query all-ids credentials out-dir]
   (runtime/log-strings "Run query:" query)
   (let [token (parse-token credentials)
         q (check-ids query all-ids)
         complete-query {:query (:query q) :name (:name q) :version (:api-version q)}
-        run-with-divided-ids #(run-nested-query-per-id token out-dir complete-query)
-        run-with-all-ids #(run-nested-query token out-dir complete-query)]
+        run-with-page-token #(run-nested-query-with-page-token token out-dir complete-query)
+        run-with-user-token #(run-nested-query token out-dir complete-query)
+        run-query #(if (need-page-token? query) (run-with-page-token) (run-with-user-token))]
     (case (:type query)
-      "nested-query" (if (ex-fb?) (run-with-divided-ids) (run-with-all-ids)))))
+      "nested-query" (run-query))))
