fa4c6a0908ea288a18321f71c707baa4d310d216
Tomas Kacur <tomas.kacur@keboola.com>
2018-08-08 17:02:39 +0200

    dont push image to quay.io registry

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag ${KBC_APP_REPOSITORY} quay.io/${KBC_APP_REPOSITORY}:${TRAVIS_TAG}
-docker tag ${KBC_APP_REPOSITORY} quay.io/${KBC_APP_REPOSITORY}:latest
-docker images
-docker push quay.io/${KBC_APP_REPOSITORY}:${TRAVIS_TAG}
-docker push quay.io/${KBC_APP_REPOSITORY}:latest
-
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm  \
     -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
