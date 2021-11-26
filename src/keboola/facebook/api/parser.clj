fcf44381e565300499aef4a33770546863973f3f
Tomas Kacur <tomaskacur@Tomass-MacBook-Pro.local>
2021-11-26 10:58:09 +0100

    Revert "remove unused code"
    This reverts commit 267bc79196bb7d86a1ecdd291e7392b2d8e6fe48.

--- src/keboola/facebook/api/parser.clj
+++ src/keboola/facebook/api/parser.clj
@@ -14,6 +14,14 @@
   (string/replace fields-str #"%%days:-?\d+%%"
                   #(relative-days-timestamp (re-find #"-?\d+" %))))
 
+(s/fdef nested-object?
+  :args (s/cat :object (s/or :nested (s/keys :req-un [::ds/data])
+                             :simple ::ds/fb-object))
+  :ret boolean?
+  :fn (fn [val]
+        (if (= :simple (-> val :args :object first))
+          (= false (-> val :ret))
+          (= true (-> val :ret)))))
 (defn nested-object?
   "Returns true if objet is map and contains :data keyword"
   [object]
@@ -26,6 +34,10 @@
      :parent-id (or (:id row) (:parent-id params))
      :fb-graph-node (str (:fb-graph-node params) "_" object-name)}))
 
+(s/fdef get-nested-objects
+  :args (s/cat :body-data ::ds/data
+               :params ::ds/keboola)
+  :ret (s/coll-of map? :into []))
 (defn get-nested-objects
   "Traverse body-data array and take out nested-object like structures.
   Return array of objects with keys :name :data :parent-id :fb-graph-node "
@@ -46,6 +58,11 @@
        memo))
    [] body-data))
 
+(s/fdef flatten-value-object
+  :args (s/cat :key1 string?
+               :object-value (s/or :scalar ::ds/table-value :object ::ds/simple-object))
+  :ret (s/* ::ds/key1-key2-value))
+
 (defn flatten-value-object
   "flattens object values and prepends and prepend key1"
   [key1 object-value]
@@ -53,6 +70,11 @@
     (map? object-value) (map (fn [[key2 val]] {:key1 key1 :key2 (name key2) :value val})
                              object-value)
     :else (list {:key1 key1 :key2 "" :value object-value})))
+
+(s/fdef flatten-array-value
+  :args (s/cat :item (s/or :scalar ::ds/table-value :object ::ds/complex-object)
+               :end_time ::ds/end_time)
+  :ret (s/* (s/merge (s/keys :req-un [::ds/end_time]) ::ds/key1-key2-value)))
 
 (defn flatten-array-value [item end_time]
   (map
@@ -87,11 +109,19 @@
         (and (= array-name :media) (empty? array)) '()
         :else (app-error (str "unsuported array:" array-name array))))
 
+(s/fdef filter-scalars
+  :args (s/cat :row (s/map-of keyword? (s/or :scalar ::ds/table-value
+                                             :object map?
+                                             :seq sequential?)))
+  :ret (s/map-of keyword? ::ds/table-value))
 (defn filter-scalars [row]
   (into {} (filter (fn [[k v]]
                      (and (-> v map? not) (-> v sequential? not)))
                    row)))
 
+(s/fdef filter-flatten-objects
+  :args (s/cat :row ::ds/complex-object)
+  :ret (s/map-of keyword? ::ds/table-value))
 (defn filter-flatten-objects
   [row]
   (let [simple-objects (filter (fn [[k v]] (and (-> v map?) (-> v nested-object? not))) row)]
